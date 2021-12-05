64a71add5eaafcf6d921799c876d3b67a665e52b
Ted <deltj@outlook.com>
2021-12-05 16:50:45 -0500

    day 1 part 1

---
+++ day1/day1_part1.cpp
@@ -0,0 +1,53 @@
+/**
+ * Advent of Code 2021
+ * Day 1 - Part 1
+ * 
+ * Given a list of numbers (sonar depth measurements), determine how many
+ * measurements increased from the previous measurement. 
+ */
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+
+#include <boost/lexical_cast.hpp>
+
+int main(int argc, char *argv[])
+{
+    if(argc != 2)
+    {
+        std::cout << "Usage: day1a <input>" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    std::ifstream ifs(argv[1], std::ifstream::in);
+    std::string line;
+    int depth_measurement = 0;
+    int previous_depth_measurement = 0;
+    int inc = 0;
+
+    //  Read the input file one line at a time
+    while(std::getline(ifs, line))
+    {
+        previous_depth_measurement = depth_measurement;
+
+        depth_measurement = boost::lexical_cast<int>(line);
+        std::cout << depth_measurement << std::endl;
+
+        //  Skip increasing/decreasing check for first measurement
+        if(previous_depth_measurement == 0)
+        {
+            continue;
+        }
+
+        if(depth_measurement > previous_depth_measurement)
+        {
+            inc++;
+        }
+    }
+    ifs.close();
+
+    std::cout << "Number of increasing measurements: " << inc << std::endl;
+
+    return EXIT_SUCCESS;
+}
